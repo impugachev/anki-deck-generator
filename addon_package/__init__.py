3897896cb6c82b3ae48e2108831850f6f8d095d8
Igor Pugachev <wotwbb@gmail.com>
2025-04-13 13:57:48 +0200

    remove deps

--- addon_package/__init__.py
+++ addon_package/__init__.py
@@ -2,6 +2,6 @@ import sys
 from os.path import dirname
 
 sys.path.append(dirname(__file__))
-sys.path.insert(0, dirname(__file__) + '/dependencies')
+sys.path.append(dirname(__file__) + '/dependencies')
 
 from . import anki_deck_generator
